1d81bca34628feafcf5a956fa554d07563550a58
pranavgain05 <pranavgain1992@gmail.com>
2026-03-24 08:49:54 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,16 @@
 import asyncio
 import telebot
 from twscrape import API, gather
+import os
 
-bot = telebot.TeleBot("8715379481:AAH3DDvgE_53hGuO3ZgMbFfk-DdXLnZt2zM")
+bot = telebot.TeleBot(os.getenv("8762357437:AAFAHjLWWZJ2Gq3AoYPJbe9wDX08k-HmAjE"))
 api = API()
 
-WORDS = ["coupon", "discount", "code", "prop firm"]
+WORDS = [
+    "coupon", "discount code", "discount", "100% discount", 
+    "100% discount code", "100% discount coupon", "free prop account", 
+    "free funded account", "free funded prop account", "prop firm discount"
+]
 
 last_id = 0
 
@@ -17,7 +22,7 @@ async def main():
         posts = await gather(api.search(query, limit=20))
         for p in posts:
             if p.id > last_id:
-                bot.send_message("YOUR_CHAT_ID", f"{p.url}\n{p.rawContent}")
+                bot.send_message(os.getenv("CHAT_ID"), f"{p.url}\n{p.rawContent}")
                 last_id = max(last_id, p.id)
         await asyncio.sleep(5)
 
